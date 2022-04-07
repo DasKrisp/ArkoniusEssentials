432a46c60378ef2716cc21aeb3d0c8835cfca62c
DasKrisp <99538764+DasKrisp@users.noreply.github.com>
2022-04-07 14:36:44 +0200

    Changed Mixin from ServerListScreen to ConnectScreen

--- src/main/java/de/krisp/arkoessentails/mixin/ConnectScreenMixin.java
+++ src/main/java/de/krisp/arkoessentails/mixin/ConnectScreenMixin.java
@@ -2,15 +2,15 @@ package de.krisp.arkoessentails.mixin;
 
 import de.krisp.arkoessentails.functions.CheckItemdamageFunction;
 import de.krisp.arkoessentails.functions.CheckServerFunction;
-import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.screen.ConnectScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MultiplayerScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("TAIL"), method = "init()V")
+@Mixin(ConnectScreen.class)
+public class ConnectScreenMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
 		CheckServerFunction.enabled = true;
 		if (CheckItemdamageFunction.scheduler != null)
